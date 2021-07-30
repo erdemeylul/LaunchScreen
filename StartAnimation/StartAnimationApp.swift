ddac4d008c129173e35015643565668d560484ea
erdemeylul <mensuckmankindrocks@gmail.com>
2021-07-30 09:26:05 +0300

    Initial Commit

---
+++ StartAnimation/StartAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartAnimationApp.swift
+//  StartAnimation
+//
+//  Created by Erdem Senol on 30.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct StartAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
